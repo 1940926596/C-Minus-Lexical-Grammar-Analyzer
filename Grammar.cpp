a4234bf025f7d6aad76aee22a32727d662441e15
Aerial <1940926596@qq.com>
2023-05-17 18:59:38 +0800

    setup

---
+++ Grammar.cpp
@@ -0,0 +1,7 @@
+//
+// Created by 19409 on 2023/5/10.
+//
+#include "Grammar.h"
+
+using namespace std;
+
--- Grammar.h
+++ Grammar.h
@@ -5,4 +5,15 @@
 #ifndef LEXICALDFA_GRAMMAR_H
 #define LEXICALDFA_GRAMMAR_H
 
+#include<string>
+#include<unordered_map>
+#include<fstream>
+
+using namespace std;
+
+class Grammar{
+
+};
+
+
 #endif //LEXICALDFA_GRAMMAR_H
